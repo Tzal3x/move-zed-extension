9082852e1f25071a1c561ef3a475d93f1ab1e661
Peter Tripp <petertripp@gmail.com>
2024-12-17 13:22:20 -0500

    Update tree-sitter queries (highlights.scm, locals.scm) to match upstream.

--- languages/move/locals.scm
+++ languages/move/locals.scm
@@ -1,17 +1,53 @@
-; Define the scope of a function based on function definition
-((function_definition
-  name: (identifier) @local.definition))
+; Function Scope
+; (function_definition
+;   body: (block) @scope
+;   parameters: (function_parameters (function_parameter name: (variable_identifier) @definition.var)))
 
-; Parameters in a function are defined as local variables
-(function_parameters
-  (function_parameter
-    (identifier) @local.definition))
+(function_definition body: (block) @scope)
 
-; When a variable is assigned a value, it's a definition
-; Assuming assignments are handled within expressions or specific assignment structures in Move
-(let_statement
-  name: (identifier) @local.definition)
+(function_parameter name: (variable_identifier) @definition.var)
+(function_parameter name: (variable_identifier) @local.definition)
 
-; Reference to identifiers within the scope of expressions
-; This general case assumes usage of variables within expressions or conditions
-((identifier) @local.reference)
+(identifier) @local.reference
+
+
+; Module and Struct Scope
+(module_definition
+  (module_body) @scope)
+
+; (struct_definition
+;   (struct_def_fields) @scope)
+
+; Spec Block Scope
+(spec_block
+  body: (spec_body) @scope)
+
+; Local Variable Declarations in Function Blocks
+; (let_statement
+;   binds: (bind_list
+;            (bind_var (variable_identifier) @definition.var)
+;            (bind_unpack
+;              (bind_field (field_identifier) @definition.var))))
+
+; Local Variable Declarations in Spec Blocks
+; (spec_block
+;   (spec_variable name: (identifier) @definition.var))
+
+; Parameters in Spec Functions
+; (spec_function
+;   parameters: (function_parameters (function_parameter name: (identifier) @definition.var)))
+;
+; ; Type Parameters
+; (type_parameters
+;   (type_parameter name: (identifier) @definition.type)))
+;
+; ; Struct Fields
+; (struct_definition
+;   (struct_def_fields
+;     (field_annotation
+;       field: (identifier) @definition.field)))
+;
+; ; Function and Module Identifiers
+; (function_definition name: (identifier) @definition.function)
+; (module_definition name: (identifier) @definition.module)
+;
