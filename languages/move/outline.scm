893fdebcaafc129b5d24fe4081cf4d70efae42ea
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2025-03-12 20:17:02 +0200

    Include const to outline

--- languages/move/outline.scm
+++ languages/move/outline.scm
@@ -1,3 +1,5 @@
 (struct_definition "struct" @context name: (_) @name) @item
 
 (function_definition "fun" @context name: (function_identifier)  @name @function) @item
+
+(constant "const" @context name: (constant_identifier)  @name @constant.name) @item
