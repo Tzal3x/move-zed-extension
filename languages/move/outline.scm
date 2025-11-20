3debc619ffd9738af09dee1b479c4bb6e7c438e1
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2025-11-20 16:17:16 +0200

    Improve outline panel view (#9)
    * Improve error message when LSP not found
    
    * Outline: recognize modifiers on Move functions
    
    * Outline: include enums
    
    * Outline: include macros
    
    * Outline: include modules
    
    * Include struct attributes and enum variants
    
    * Outline: include types of fields in enums and structs
    
    * Bump extension version

--- languages/move/outline.scm
+++ languages/move/outline.scm
@@ -1,5 +1,15 @@
 (struct_definition "struct" @context name: (_) @name) @item
 
-(function_definition "fun" @context name: (function_identifier)  @name @function) @item
+(field_annotation field: (field_identifier) @name ":" @context type: (_) @context) @item
+
+(function_definition (modifier)? @context "fun" @context name: (function_identifier) @name) @item
 
 (constant "const" @context name: (constant_identifier)  @name @constant.name) @item
+
+(enum_definition "enum" @context name: (_) @name) @item
+
+(macro_function_definition (modifier)* @context "macro" @context "fun" @context name: (function_identifier) @name) @item
+
+(module_definition "module" @context module_identity: (_) @name) @item
+
+
