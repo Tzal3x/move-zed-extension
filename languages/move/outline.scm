2393bd6aa2825254430aabf742c164b8164060aa
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-11-20 15:13:52 +0200

    Outline: recognize modifiers on Move functions

--- languages/move/outline.scm
+++ languages/move/outline.scm
@@ -1,5 +1,5 @@
 (struct_definition "struct" @context name: (_) @name) @item
 
-(function_definition "fun" @context name: (function_identifier)  @name @function) @item
+(function_definition (modifier)? @context "fun" @context name: (function_identifier) @name) @item
 
 (constant "const" @context name: (constant_identifier)  @name @constant.name) @item
