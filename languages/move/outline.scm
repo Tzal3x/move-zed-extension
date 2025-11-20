944f9c9ab73d903affb9c45333aee221604a3d38
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-11-20 16:00:39 +0200

    Include struct attributes and enum variants

--- languages/move/outline.scm
+++ languages/move/outline.scm
@@ -1,5 +1,7 @@
 (struct_definition "struct" @context name: (_) @name) @item
 
+(field_annotation field: (field_identifier) @name) @item
+
 (function_definition (modifier)? @context "fun" @context name: (function_identifier) @name) @item
 
 (constant "const" @context name: (constant_identifier)  @name @constant.name) @item
@@ -9,3 +11,5 @@
 (macro_function_definition (modifier)* @context "macro" @context "fun" @context name: (function_identifier) @name) @item
 
 (module_definition "module" @context module_identity: (_) @name) @item
+
+
