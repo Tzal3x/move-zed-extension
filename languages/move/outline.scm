ada84a5162371f0d51eaf47714692d43eed5f7af
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-11-20 15:34:53 +0200

    Outline: include enums

--- languages/move/outline.scm
+++ languages/move/outline.scm
@@ -3,3 +3,5 @@
 (function_definition (modifier)? @context "fun" @context name: (function_identifier) @name) @item
 
 (constant "const" @context name: (constant_identifier)  @name @constant.name) @item
+
+(enum_definition "enum" @context name: (_) @name) @item
